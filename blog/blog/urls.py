d4d7f5ae60b423a15e8a3669c6af61b3ff3eeed0
Hueso de Durazno <ivanwhite230@gmail.com>
2022-04-11 19:31:27 -0500

    agregamos rutas de usuario , create , get y put

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
@@ -36,4 +37,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/',schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/',schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('users.api.router')),
 ]
